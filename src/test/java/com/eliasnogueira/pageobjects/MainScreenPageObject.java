9081fe287afaf9968c2fd48eb9cd589f484447a7
jpita <dvpita@gmail.com>
2018-12-06 15:30:50 +0100

    changing selectors

--- src/test/java/com/eliasnogueira/pageobjects/MainScreenPageObject.java
+++ src/test/java/com/eliasnogueira/pageobjects/MainScreenPageObject.java
@@ -13,45 +13,34 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.eliasnogueira.po;
-
-import java.util.concurrent.TimeUnit;
-
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
+package com.eliasnogueira.pageobjects;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
+import io.appium.java_client.pagefactory.AndroidBy;
 import io.appium.java_client.pagefactory.AndroidFindBy;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
-import io.appium.java_client.pagefactory.WithTimeout;
-import io.appium.java_client.pagefactory.iOSFindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class MainScreenPageObject {
 
 	@AndroidFindBy(id = "org.traeg.fastip:id/billAmtEditText")
-	@iOSFindBy(xpath = "//XCUIElementTypeApplication[1]/XCUIElementTypeWindow[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]/XCUIElementTypeTextField[1]")
-	@WithTimeout(time = 10, unit = TimeUnit.SECONDS)
 	MobileElement billAmount;
 	
 	@AndroidFindBy(id = "org.traeg.fastip:id/calcTipButton")
-	@iOSFindBy(xpath = "//XCUIElementTypeApplication[1]/XCUIElementTypeWindow[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]/XCUIElementTypeButton[1]")
-	@FindBy(id = "calcTip")
 	MobileElement calculateTip;
 
 	@AndroidFindBy(id = "org.traeg.fastip:id/tipAmtTextView")
-	@iOSFindBy(xpath = "//XCUIElementTypeApplication[1]/XCUIElementTypeWindow[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]/XCUIElementTypeStaticText[2]")
-	@WithTimeout(time = 10, unit = TimeUnit.SECONDS)
 	MobileElement tipAmount;
 
 	@AndroidFindBy(id = "org.traeg.fastip:id/totalAmtTextView")
-	@iOSFindBy(xpath = "//XCUIElementTypeApplication[1]/XCUIElementTypeWindow[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]/XCUIElementTypeStaticText[4]")
-	@WithTimeout(time = 10, unit = TimeUnit.SECONDS)
 	MobileElement totalAmount;
 
-	public MainScreenPageObject(AppiumDriver<?> driver) {
-		PageFactory.initElements(new AppiumFieldDecorator(driver), this);
-	}
+    @AndroidBy(id = "org.traeg.fastip:id/calcTipButton")
+
+    public MainScreenPageObject(AppiumDriver<?> driver) {
+        PageFactory.initElements(new AppiumFieldDecorator(driver), this);
+    }
 
 	public void fillBillAmount(String amount) {
 		billAmount.sendKeys(amount);
